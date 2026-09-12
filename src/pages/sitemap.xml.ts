b981e20181970aad9fd21ca864555422df43e1f2
orima1995-create <orima1995@gmail.com>
2026-09-13 08:37:00 +0900

    Remove noindex smartwatch finale from sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -7,7 +7,6 @@ export async function GET() {
     root,
     `${root}history/`,
     `${root}owners-notes/`,
-    `${root}history/smartwatch/`,
     ...watches.filter((item) => item.data.published).map((item) => `${root}${item.data.slug}/`)
   ];
   const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls.map((url) => `  <url><loc>${url}</loc></url>`).join('\n')}\n</urlset>\n`;
